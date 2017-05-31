cb3be9489fbeaf41eb02f144cf6eb973f7912e92
Matthieu Estrada <ttamalfor@gmail.com>
2017-05-31 09:56:50 +0200

    Add translate for EN and FR

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -6,3 +6,6 @@
 $lang['encoding']   = 'utf-8';
 $lang['direction']  = 'ltr';
 
+$lang['gitlab.project'] = 'Gitlab Project';
+$lang['gitlab.activity'] = 'Last activity';
+$lang['gitlab.members'] = 'Members';
--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -5,3 +5,8 @@
 
 $lang['encoding']   = 'utf-8';
 $lang['direction']  = 'ltr';
+
+$lang['gitlab.project'] = 'Projet Gitlab';
+$lang['gitlab.activity'] = 'Derni&egrave;re activit&eacute;';
+$lang['gitlab.members'] = 'Membres';
+
