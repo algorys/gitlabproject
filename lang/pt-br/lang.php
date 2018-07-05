08176986ef2dee9f0a4c9cad8456fe3fe997b106
Alexandre Belchior <alexbelchior@gmail.com>
2018-07-05 13:50:31 +0200

    translation update

---
+++ lang/pt-br/lang.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Alexandre Belchior <alexbelchior@gmail.com>
+ */
+$lang['encoding']              = 'utf-8';
+$lang['direction']             = 'ltr';
+$lang['gitlab.project']        = 'Projeto Gitlab';
+$lang['gitlab.activity']       = 'Última atividade';
+$lang['gitlab.members']        = 'Membros';
+$lang['gitlab.error']          = '<span style = "color: red;"> O GitlabProject não pode encontrar seu projeto! </ span> Por favor, verifique seu <b> namespace </ b> e seu <b> nome do projeto </ b>. Certifique-se de configurar o servidor certo e o token certo!';
