6c42d55526d9d38e0faa2b26e0b590cc91f1ea49
Frank <frank.feldhoff@mailbox.org>
2019-01-14 13:25:25 +0100

    translation update

---
+++ lang/de/lang.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Frank <frank.feldhoff@mailbox.org>
+ */
+$lang['encoding']              = 'UTF-8';
+$lang['direction']             = 'ltr';
+$lang['gitlab.project']        = 'Gitlab Project';
+$lang['gitlab.activity']       = 'Letzte Aktivität';
+$lang['gitlab.members']        = 'Mitglieder';
+$lang['gitlab.error']          = '<span style="color: red;">Das Gitlab Projekt kann Ihr Projekt nicht finden!</span> Bitte überprüfen Sie Ihr <b>namespace</b> und Ihren<b>project name</b>. Stellen Sie sicher das der korrekte Server eingestellt und das Token gültig ist!';
---
+++ lang/de/settings.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Frank <frank.feldhoff@mailbox.org>
+ */
+$lang['server.default']        = 'Fügen Sie die URL Ihres Gitlab Servers (ohne "/" am Ende) ein. Beispiel: http://my-gitlab.com ';
+$lang['token.default']         = 'Ein gültiges Token, das zugriff auf alle Projekte hat.';
+$lang['unwanted.users']        = 'Falls Sie einige Nutzer Ihres Projekts nicht angezeigt bekommen wollen, fügen Sie diese hier getrennt durch Kommata ein.';
