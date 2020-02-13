c39cb3e4d595d5fa93358e5c4e1b800377c739c1
Aleksandr Selivanov <alexgearbox@yandex.ru>
2020-02-13 20:25:31 +0100

    translation update

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,13 +1,15 @@
 <?php
-/*
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
  * English language file
+ *
+ * @author Aleksandr Selivanov <alexgearbox@yandex.ru>
  */
-
-$lang['encoding']   = 'utf-8';
-$lang['direction']  = 'ltr';
-
-$lang['gitlab.project'] = 'Gitlab Project';
-$lang['gitlab.activity'] = 'Last activity';
-$lang['gitlab.members'] = 'Members';
-
-$lang['gitlab.error'] = '<span style="color: red;">GitlabProject can\'t find your project !</span> Please check your <b>namespace</b> and your <b>project name</b>. Make sure you set up the right server and the right token !';
+$lang['encoding']              = 'utf-8';
+$lang['direction']             = 'ltr';
+$lang['gitlab.project']        = 'Gitlab Project';
+$lang['gitlab.activity']       = 'Last activity';
+$lang['gitlab.members']        = 'Members';
+$lang['gitlab.error']          = '<span style="color: red;">GitlabProject can\'t find your project!</span> Please check your <b>namespace</b> and your <b>project name</b>. Make sure you set up the right server and the right token!';
--- lang/en/settings.php
+++ lang/en/settings.php
@@ -1,7 +1,12 @@
 <?php
-/*
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
  * Settings language file
+ *
+ * @author Aleksandr Selivanov <alexgearbox@yandex.ru>
  */
-$lang['server.default'] = 'Add your Gitlab serveur url, without the slash ending. E.g.: http://my-gitlab.com';
-$lang['token.default'] = 'A valid token who has access to all projects.';
-$lang['unwanted.users'] = 'If you do not want to show some users of your projects, add them here, separated by commas.';
+$lang['server.default']        = 'Add your Gitlab server url, without the slash ending. E.g.: http://my-gitlab.com';
+$lang['token.default']         = 'A valid token who has access to all projects.';
+$lang['unwanted.users']        = 'If you do not want to show some users of your projects, add them here, separated by commas.';
