85ace2569f68d6932ad48c208024cddfbc50ae2a
Aleksandr Selivanov <alexgearbox@yandex.ru>
2020-02-13 20:50:18 +0100

    translation update

---
+++ lang/ru/lang.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Aleksandr Selivanov <alexgearbox@yandex.ru>
+ */
+$lang['encoding']              = 'utf-8';
+$lang['direction']             = 'ltr';
+$lang['gitlab.project']        = 'Gitlab-проект';
+$lang['gitlab.activity']       = 'Активность';
+$lang['gitlab.members']        = 'Участники';
+$lang['gitlab.error']          = '<span style="color: red;">GitlabProject не может найти ваш проект!</span> Проверьте, пожалуйста, правильность <b>автора или группы проекта</b> и <b>имени проекта.</b> Убедитесь, что верно указали сервер и токен!';
